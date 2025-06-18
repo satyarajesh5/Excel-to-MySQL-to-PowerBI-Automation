2b3bfc874f0fa6205b5e0c3f6e8ed0c0d5643d7c
Satya Rajesh Veeravilli <veeravillisatyarajesh@gmail.com>
2025-06-18 21:09:02 +0530

    upload python file

---
+++ PythonScript/ExceltoDB.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sqlalchemy import create_engine as ce, text
+from datetime import datetime
+import SenstiveData
+import yagmail
+# map excels and DB Tables
+file_table_map={
+    "Sales_info.xlsx" : "sales_table",
+    "Price_info.xlsx" : "price_table"
+}
+
+# Map Date columns from tables
+date_map={
+    "Sales_info.xlsx" : ["Date"],
+    "Price_info.xlsx" : ["Date"]
+}
+data = None
+# create a engine to create a connection to Database
+engine = ce(f"mysql+mysqlconnector://{SenstiveData.user}:{SenstiveData.password}@{SenstiveData.host}/{SenstiveData.database}")
+with engine.begin() as conn:
+    for file_name, table_name in file_table_map.items():
+        #read files to pandas
+        df = pd.read_excel(file_name)
+
+        #format date as per mysql
+        for col in date_map.get(file_name,[]):
+            df[col]=pd.to_datetime(df[col],format="%d-%m-%Y", errors="coerce")
+            data = df
+        #Truncate the tables
+        conn.execute(text(f"TRUNCATE TABLE {table_name}"))
+
+        df.to_sql(table_name,con=engine,if_exists="append",index=False)
+
+        print(f"{file_name} is updated in the table called {table_name}")
+data.to_excel("Data.xlsx", index=False)
+content = "Data.xlsx"
+email = yagmail.SMTP(
+    user="satyarajesh566@gmail.com", password="njyn yqul cbxd eiuf"
+        )
+
+email.send(
+            to="veeravillisatyarajesh@gmail.com",
+            subject="task receiving check",
+            contents=content
+        )
+print("All tables successfully importated")
+
